a1241cb44e8e64647d9d7ab8f6e3353d8e8db858
AYSOAT <66264981+AYSOAT-Ltd@users.noreply.github.com>
2020-06-26 16:12:52 +0530

    Add files via upload

---
+++ ODD OR EVEN.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	printf("ENTER THE NUMBER TO KNOW WHETHER IT IS ODD OR EVEN!!..\n");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("THE NUMBER ENTERED IS EVEN!\n");
+	}
+	else
+	{
+		printf("THE NUMBER ENTERED IS ODD...\n");
+	}
+}
